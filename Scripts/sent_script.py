f5029fb4b9a4ae80a933fe024357aa2429eb287b
Student <maximilianfuksik@gmail.com>
2026-08-17 23:31:14 +0200

    Python Sent-Script Version 1.2

--- Scripts/sent_script.py
+++ Scripts/sent_script.py
@@ -23,7 +23,7 @@ def main():
     print(f"-" * 69)
     
     # Verify local source directory exists
-    if not os.path.exists{SOURCE_DIR}:
+    if not os.path.exists(SOURCE_DIR):
         print(f"[ERROR] directory was not found")
         return
     
@@ -39,9 +39,59 @@ def main():
         ssh_transport.connect(username=BACKUP_USER, pkey=key)
         sftp = paramiko.SFTPClient.from_transport(ssh_transport)
         
-        #Upload all files from the local directory loop 
-        for filename in os.listdir(SOURCE_DIR):
-            local_file = os.path.join(SOURCE_DIR, filename)
+        #Upload : os.walk loops through ALL subdirectories, folders, and files
+        for root, dirs, files in os.walk(SOURCE_DIR):
+            for filename in files:
+                
+                local_file_path= os.path.join(root, filename)
+                
+                relative_path = os.path.realpath(local_file_path, SOURCE_DIR)
+                
+                # Format the remote path for Linux 
+                remote_file_path = f"/home/{BACKUP_USER}/{relative_path}".replace('\\','/')
+                remote_dir = os.path.dirname(remote_file_path)
+                
+                try: 
+                    sftp.stat(remote_dir)
+                except IOError:
+                    
+                    parts = remote_dir.split('/')
+                    current_path = ""
+                    for part in parts:
+                        if part:
+                            current_path += "/" + part
+                            try: 
+                                sftp.stat(current_path)
+                            except IOError:
+                                sftp.mkdir(current_path)
+                
+                sftp.put(local_file_path, remote_file_path)
+                print(f" Uploades: {relative_path}")
+                
+        sftp.close()
+        ssh_transport.close()
+
+        print("-" * 69)
+        print("[*] Transfer complete. Triggering server-side processing...")
+
+        ssh_admin = paramiko.SSHClient()
+        ssh_admin.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_admin.connect(SERVER_IP, username=ADMIN_USER, pkey=key)
+
+        stdin, stdout, stderr = ssh_admin.exec_command(f"sudo bash /home/{ADMIN_USER}/backup_process.sh")
+        exit_status = stdout.channel.recv_exit_status()
+
+        if exit_status == 0: 
+            print("-" * 69)
+            print("[SUCCESS]")
+        else:
+            print("-" * 69)
+            print("[ERROR] Server-side processing failed")
         
-    except:
-        pass
+    except Exception as e:
+        print("-" * 69)
+        print(f"[Critical ERROR] Pipeline failed: {e}")
+        
+        
+if __name__ == "__main__":
+    main()
